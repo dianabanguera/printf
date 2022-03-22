643182008ce7242f455520e712a77fa5a80bffbd
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-22 12:21:16 -0700

    Agrega putchar a main.h

--- main.h
+++ main.h
@@ -16,7 +16,8 @@ typedef struct print
 	int (*m)(va_list);
 } print_p;
 
-int _printf(const char *format, ...);
+int _printf(const char 	*format, ...);
+int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
