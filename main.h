2c402d977d0e228b85508abf9f12e9f6b817841e
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-22 09:00:02 -0700

    Main

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h> 
+#include <stdlib.h>
+
+/**
+ * struct print - This structure, prints diferent types
+ * @p: type to print
+ * @m: function to print
+ */
+typedef struct print
+{
+	char *p;
+	int (*m)(va_list);
+} print_p;
+
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+
+#endif
