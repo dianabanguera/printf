09090b272c11cafb3b0db9736cdd05833636684a
Mr-emilio <pablopeag@hotmail.com>
2022-03-23 10:54:27 -0500

    fixed bugs

--- main.h
+++ main.h
@@ -18,14 +18,14 @@ typedef struct print
 	int (*m)(va_list);
 } print_p;
 
-int _printf(const char 	*format, ...);
+int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
 int print_percent(va_list %);
-int (*specifiers(const char *format)(va_list);
+int (*specifiers(const char *format))(va_list);
 int _strlen_recursion(char *s);
 
 #endif
