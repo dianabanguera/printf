bdcb36d9173bd927995d7317291607b05cf0ec25
Mr-emilio <pablopeag@hotmail.com>
2022-03-23 17:14:37 -0500

    printf main

---
+++ printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * _printf - prints anything according to a format
+ * @format: format selec
+ * Return: numbers of characters
+ */
+int _printf(const char *format, ...)
+{
+	int index = 0, contador = 0;
+	va_list object;
+	int (*funciones)(va_list);
+
+	va_start(object, format);
+	while (format[index])
+	{
+		if (format[index] == '%' && format[index + 1] == '%')
+		{
+			_putchar('%');
+			contador++;
+			index += 2;
+			continue;
+		}
+		else if (format[index] == '%')
+		{
+			funciones = specifiers(&format[index + 1]);
+			if (funciones != NULL)
+			{
+				contador += funciones(object);
+				index += 2;
+				continue;
+			}
+			else
+			{
+				if (format[index] == '%' && format[index + 1] == '\0')
+				{
+					contador++;
+					_putchar(format[index]);
+				}
+			}
+		}
+		else if (format[index] != '\0')
+		{
+			_putchar(format[index]);
+			contador++;
+		}
+		index++;
+	}
+	va_end(object);
+	return (contador);
+}
+
+
+
