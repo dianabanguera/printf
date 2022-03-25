05a55a29af532fa3d4fc989ab1e606910933f848
Mr-emilio <pablopeag@hotmail.com>
2022-03-25 00:41:24 -0500

    numbers

---
+++ printnum.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * count - cuenta tamaño 
+ * @number: number
+ * Return: degits of number
+ *
+ */
+int count(int number)
+{
+	unsigned int num = 0, cont;
+
+	if (number < 0)
+		cont = number * -1;
+	else
+		cont = number;
+	while (cont != 0)
+	{
+		cont /= 10;
+		num++;
+	}
+	return (num);
+}
+/**
+ * print_num - print number
+ * @num: number to print
+ */
+int print_num(va_list num)
+{
+	unsigned int comparator;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		comparator = -num;
+	}
+	else
+		comparator = num;
+
+	if (comparator / 10)
+		print_num(comparator / 10);
+	_putchar((comparator % 10) + '0');
+}
+/**
+ *
+ */
+int print_int(va_list id)
+{
+       int num = va_arg(id, int);
+       int cont = count(num);
+
+       if (n <= 0)
+       {
+	       cont++;
+	       print_number(num);
+       }
+       return (cont);
+}
